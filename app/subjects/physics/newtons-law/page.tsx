c4e91db43a3c4cabda5527677bb8329f88dfedfe
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-02-02 21:06:04 +0530

    Fix import path for SubjectPage component

--- app/subjects/physics/newtons-law/page.tsx
+++ app/subjects/physics/newtons-law/page.tsx
@@ -1,4 +1,4 @@
-import SubjectPage, { Topic } from "../../../../components/SubjectPage";
+import SubjectPage, { Topic } from "../../../components/SubjectPage";
 
 
 const newtonsLawsTopics: Topic[] = [
